11c97606f28ec546f3cfa75b0adc13688648473b
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-12-22 18:06:15 -0500

    Created router logic for get, getById, edit, and delete (need to think about getting dogs/cats, will come back to those).

---
+++ pets/pets-router.js
@@ -0,0 +1,77 @@
+const express = require('express');
+
+const db = require('../data/db-config');
+
+const Pets = require('./pets-model.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Pets.find()
+  .then(pets => res.json(pets))
+  .catch(err => res.status(500).json({ message: "Failed to get pets"}))
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    Pets.findById(id)
+        .then(pet => {
+            if (pet) {
+                res.json(scheme);
+            } else {
+                res.status(404).json({ message: 'Could not find pet with given id.' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get pets' });
+        });
+});
+
+router.post('/', (req, res) => {
+    const petData = req.body;
+  
+    Pets.add(petData)
+        .then(pet => {
+            res.status(201).json(pet);
+        })
+        .catch (err => {
+            res.status(500).json({ message: 'Failed to create new pet' });
+        });
+  });
+
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+  
+    Pets.findById(id)
+        .then(pet => {
+            if (pet) {
+                Pets.update(changes, id)
+                    .then(updatedPet => {
+                        res.json(updatedPet);
+                    });
+            } else {
+                res.status(404).json({ message: 'Could not find pet with given id' });
+            }
+        })
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to update pet' });
+    });
+  });
+
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    Pets.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.json({ removed: deleted });
+            } else {
+                res.status(404).json({ message: 'Could not find pet with given id' });
+            }
+        })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to delete pet' });
+    });
+  });
